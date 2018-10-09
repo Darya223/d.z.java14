2caff5ddeb32b9f24cdfe93205baff53ad61cb6f
Сексуальный пингвин <ghh@mail.ru>
2018-10-09 12:57:54 +0300

    Initial commit

---
+++ src/org/itstep/qa/runner/RunnerBush.java
@@ -0,0 +1,27 @@
+package org.itstep.qa.runner;
+
+import org.itstep.qa.zadachi.Bush;
+
+public class RunnerBush {
+    public static void main(String[] args) {
+
+        Bush bush1 = new Bush(55,23,"синий");
+        Bush bush2 = new Bush(89,28,"голубой");
+        Bush bush3 = new Bush(1,25,"фиолетовый");
+        Bush bush4 = new Bush(92,76,"зелёный");
+        Bush bush5 = new Bush(53,20,"чёрный");
+        Bush bush6 = new Bush(54,47,"серый");
+        Bush bush7 = new Bush(12,70,"жёлтый");
+        Bush bush8 = new Bush(56,23,"красный");
+        Bush bush9 = new Bush(76,17,"розовый");
+        Bush bush10 = new Bush(14,93,"оранжевый");
+        Bush bush11 = new Bush(82,60,"сиреневый");
+        Bush bush12 = new Bush(3,51,"коричневый");
+        Bush bush13 = new Bush(89,84,"бордовый");
+        Bush bush14 = new Bush(61,19,"белый");
+        Bush bush15 = new Bush(37,37,"салатовый");
+        Bush bush16 = new Bush(41,95,"бежевый");
+        Bush bush17 = new Bush(73,28,"золотой");
+        Bush bush18 = new Bush(95,69,"серебристый");
+    }
+}
---
+++ src/org/itstep/qa/zadachi/Bush.java
@@ -0,0 +1,37 @@
+package org.itstep.qa.zadachi;
+
+public class Bush {
+    private int x;            //координаты куста
+    private int y;
+    private String color;           //цвет
+
+    public Bush(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
